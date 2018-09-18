122ac499c4e3d38af725ccf47cd12c3eb8585db5
dylan gijsbertsen <d.b.gijsbertsen@live.nl>
2018-09-18 14:22:37 +0200

    Changed the BT to use a C++ class instead of BP
    * Added new C++ class called ChooseNextWaypoint
    * Changed Patrol BT
    * Removed BP ChooseNextWaypoint from Patrol BT

---
+++ Source/S03_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+
+
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask( UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory )
+{
+	Super::ExecuteTask( OwnerComp, NodeMemory );
+	UE_LOG( LogTemp, Warning, TEXT( "Donkey: C++ choose way point succeeded" ) );
+	return EBTNodeResult::Succeeded;
+}
