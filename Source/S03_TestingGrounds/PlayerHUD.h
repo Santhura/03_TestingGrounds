c38a9811304f2546efdc34759540f8e9475d7d78
dylan gijsbertsen <d.b.gijsbertsen@live.nl>
2019-03-26 12:54:59 +0100

    Added new Weapon models from [Quick and Easy]

---
+++ Source/S03_TestingGrounds/PlayerHUD.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "S03_TestingGroundsHUD.h"
+#include "PlayerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S03_TESTINGGROUNDS_API APlayerHUD : public AS03_TestingGroundsHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
